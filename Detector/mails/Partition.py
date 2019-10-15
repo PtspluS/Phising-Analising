c51a2f75c7fa931e874d1fad7d5d3b3ec1462352
Antoine Genonceau <antoine.genonceau@isen.yncrea.fr>
2019-10-15 14:34:28 +0200

    Extraction du header

--- Detector/mails/Partition.py
+++ Detector/mails/Partition.py
@@ -23,6 +23,7 @@ def partition(mail, balise):
 
     return partie;
 
+
 def body(mail):
     
     return partition(mail, "body");
@@ -35,8 +36,85 @@ def header(mail):
     
     return partition(mail, "header");
 
-
-
+def date(header):
     
+    date = re.match(r"^((.|\n)*)Date: ((.|\n)*)From", header);
+    
+    return date.group(3);
+
+def source(header):
+    
+    source = re.match(r"^((.|\n)*)From: ((.|\n)*)Subject", header);
+    
+    return source.group(3);
+
+def sourceAdresse(source):
+    
+    sourceAdresse = re.match(r"^((.|\n)*)<((.|\n)*)>", source);
+    
+    return sourceAdresse.group(3);
+
+def sourceNom(source):
+    
+    sourceNom = re.match(r"((.|\n)*) <", source);
+    
+    return sourceNom.group(1);
+
+def objet(header):
+    
+    objet = re.match(r"^((.|\n)*)Subject:((.|\n)*)Thread-Topic", header);
+    
+    return objet.group(3);
+
+def topic(header):
+    
+    topic = re.match(r"^((.|\n)*)Topic:((.|\n)*)To: ", header);
+    
+    return topic.group(3);
+
+
+def destination(header):
+    
+    destination = re.match(r"^((.|\n)*)To: ((.|\n)*)Content-Transfer-Encoding: ", header);
+    
+    return destination.group(3);
+
+def destinationAdresse(destination):
+    
+    destinationAdresse = re.match(r"^((.|\n)*)<((.|\n)*)>", destination);
+    
+    return destinationAdresse.group(3);
+
+def destinationNom(destination):
+    
+    destinationNom = re.match(r"((.|\n)*) <", destination);
+    
+    return destinationNom.group(1);
+
+
+def encodageTransfert(header):
+    
+    encodageTransfert = re.match(r"^((.|\n)*)Content-Transfer-Encoding: ((.|\n)*)Content-Type: ", header);
+    
+    return encodageTransfert.group(3);
+
+
+def typeMail(header):
+    
+    typeMail = re.match(r"^((.|\n)*)Content-Type: ((.|\n)*)$", header);
+    
+    return typeMail.group(3);
+
+def langage(typeMail):
+    
+    langage = re.match(r"((.|\n)*);", typeMail);
+    
+    return langage.group(1);
+
+def norme(typeMail):  
+    
+    norme = re.match(r"((.|\n)*); ((.|\n)*)$", typeMail, re.MULTILINE);
+    
+    return norme.group(3);
 
 
