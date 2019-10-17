4f2e5d819566ed8fd43291a5c2e10d2bfd9f1947
Grand Corbeau <qboens@hotmail.fr>
2019-10-17 14:56:02 +0200

    correction_position_fichier_sauvegarde

--- src/Recevoir_email_complet.py
+++ src/Recevoir_email_complet.py
@@ -29,7 +29,7 @@ while True:
         raw_email_string = raw_email.decode('utf-8')
         email_message = email.message_from_string(raw_email_string)
         for part in email_message.walk():
-            save_string = r"C:\\Users\\qboen\\Desktop\\02-Projet\\00-Fishing\\Phising-Analising\\src\\email" + str(x) + ".txt"
+            save_string = r"email" + str(x) + ".txt"
             myfile = open(save_string, 'a')
             myfile.write(str(part))
             myfile.close()
