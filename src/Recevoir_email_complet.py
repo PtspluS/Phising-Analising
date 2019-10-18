af3c077bd0c279d58c3ea38d22c05ed444be5fd8
Grand Corbeau <qboens@hotmail.fr>
2019-10-18 13:25:05 +0200

    repartition_email_dossier

--- src/Recevoir_email_complet.py
+++ src/Recevoir_email_complet.py
@@ -1,11 +1,28 @@
 import imaplib
 import email
+from time import sleep
+from random import randint
 
 mail = imaplib.IMAP4_SSL('imap.gmail.com')
 mail.login('yncrea.test.projet.M1@gmail.com', 'ujikolpm')
 
+def efface_old_mail():
+    import os
+
+    arret = False
+    x = 1
+    while not arret:
+        if os.path.exists("email"+str(x)+".txt"):
+          os.remove("email"+str(x)+".txt")
+        else:
+          print("Fichiers supprimés")
+          arret = True
+
 old_mes_nb = -1
 x = 1
+
+efface_old_mail()
+
 while True:
     
     mail.list() 
@@ -13,6 +30,7 @@ while True:
 
 
     result, data = mail.uid('search', None, "ALL")
+    
     i = len(data[0].split())
     new_mes_nb = i
 
@@ -21,44 +39,73 @@ while True:
     
 
     if(new_mes_nb > old_mes_nb):
-        print("NOUVEAU MESSAGE")
+        print("\n---NOUVEAU MESSAGE : %i---"%x)
         latest_email_uid = data[0].split()[new_mes_nb - 1]
+        print(latest_email_uid)
         result, email_data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-        raw_email = email_data[0][1]
 
+
+        raw_email = email_data[0][1]
+        
         raw_email_string = raw_email.decode('utf-8')
         email_message = email.message_from_string(raw_email_string)
         for part in email_message.walk():
             save_string = r"email" + str(x) + ".txt"
             myfile = open(save_string, 'a')
             myfile.write(str(part))
+            
             myfile.close()
+
+
+        
+        
+        cible_dossier = 'traitement'
+        
+        result_move, err_mes = mail.uid('move',latest_email_uid,cible_dossier)
+        if(result_move =='OK'):
+            print("Mail déplacé avec succès")
+        else:
+            print(err_mes)
+            
+        mail.select(cible_dossier)
+
+        result, data = mail.uid('search', None, "ALL")
+        print(data[0])
+        latest_email_uid = data[0].split()[- 1]
+        sleep(15)       
+
+        sortie_traitement = randint(1,3)
+
+        print("Résultat traitement :", sortie_traitement)
+
+        if(sortie_traitement == 1):
+            result_move, err_mes = mail.uid('move',latest_email_uid,"sur")
+            if(result_move =='OK'):
+                print("Mail", str(latest_email_uid), "déplacé avec succès")
+            else:
+                print(err_mes)
+
+        elif(sortie_traitement == 2):
+            result_move, err_mes = mail.uid('move',latest_email_uid,"moyen")
+            if(result_move =='OK'):
+                print("Mail", str(latest_email_uid), "déplacé avec succès")
+            else:
+                print(err_mes)
+
+        else:
+            result_move, err_mes = mail.uid('move',latest_email_uid,"danger")
+            if(result_move =='OK'):
+                print("Mail", str(latest_email_uid), "déplacé avec succès")
+            else:
+                print(err_mes)
+
+        
         x += 1
         old_mes_nb = new_mes_nb
+        print("Analyse effectuée")
     elif(new_mes_nb < old_mes_nb):
         old_mes_nb = new_mes_nb
 
-mail.list() 
-mail.select('inbox')
 
 
-result, data = mail.uid('search', None, "ALL")
-print("Contenu data :",data)
-i = len(data[0].split())
-print("Nombre messages :",i)
-new_mes_nb = i
-"""
-for x in range(i):
-    latest_email_uid = data[0].split()[x]
-    result, email_data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-    raw_email = email_data[0][1]
 
-    raw_email_string = raw_email.decode('utf-8')
-    email_message = email.message_from_string(raw_email_string)
-    
-for part in email_message.walk():
-    save_string = r"C:\\Users\\qboen\\Desktop\\02-Projet\\00-Fishing\\Phising-Analising\\src\\email" + str(x) + ".txt"
-    myfile = open(save_string, 'a')
-    myfile.write(str(part))
-    myfile.close()
-"""
