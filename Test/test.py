5ce7f3a19561f72344542b77f58615a92d510ce6
Antoine Genonceau <antoine.genonceau@isen.yncrea.fr>
2019-11-12 11:38:55 +0100

    Creation Test

---
+++ Test/test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 11:19:15 2019
+
+@author: Toine
+"""
+
+import scriptAnalyseSource;
+
+print(scriptAnalyseSource.analyseSource("mailsTest/email.txt"));
