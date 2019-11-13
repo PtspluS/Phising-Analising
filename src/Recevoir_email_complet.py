faf447d48d95f0aa1391407229d53f15eea10c33
Grand Corbeau <qboens@hotmail.fr>
2019-11-13 13:11:48 +0100

    correction_erreur_recevoir_mail

--- src/Recevoir_email_complet.py
+++ src/Recevoir_email_complet.py
@@ -14,6 +14,7 @@ def efface_old_mail():
     while not arret:
         if os.path.exists("email"+str(x)+".txt"):
           os.remove("email"+str(x)+".txt")
+          x += 1
         else:
           print("Fichiers supprimés")
           arret = True
