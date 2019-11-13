f7ac37a4b791d3192184ca627f2ec722ece28da1
PtspluS <pierre.marez@isen.yncrea.fr>
2019-11-13 16:02:11 +0100

    correct error pointer

--- src/Clustering.py
+++ src/Clustering.py
@@ -6,15 +6,12 @@ import os
 import codecs
 from sklearn import feature_extraction
 import mpld3
-from Email_extractor import extract_text_full, find_language
+from Email import Email
 
 mail = open("../mail_0.txt",'r')
-mail_text = extract_text_full(mail)
-lang = find_language(mail)
 
-if lang == 'EN':
-    pass
+email = Email(mail.read())
 
-def En_clustering():
-    pass
+print(email.get_text())
+
 
--- src/Email.py
+++ src/Email.py
@@ -13,9 +13,9 @@ class Email:
         else:
             self.type = 'html'
 
-        self.header = self.extract_header()
-        self.body = self.extract_body()
-        self.text = self.extract_text_full()
+        self.header = self.extract_header(self.email_brut)
+        self.body = self.extract_body(self.email_brut)
+        self.text = self.extract_text_full(self.email_brut)
         self.lang = self.find_language()
 
     '''
@@ -24,16 +24,17 @@ class Email:
         return : 
             - return the header (str)
     '''
-    def extract_header(self):
+    def extract_header(self, txt):
+        email_brut = (txt+'.')[:-1]
         if self.type == 'html' :
-            header = re.split("<body.*>", self.email_brut)
+            header = re.split("<body.*>", email_brut)
             return header[0]
         else :
-            if re.search("Content-Transfer-Encoding:", self.email_brut) == None :
-                header = re.split("Date : \.*\n", self.email_brut)
-                header += re.findall("Date : \.*\n", self.email_brut)[0]
+            if re.search("Content-Transfer-Encoding:", email_brut) == None :
+                header = re.split("Date:.*\n", email_brut)
+                #header += re.findall("Date:\.*\n", email_brut)[0]
             else :
-                header = re.split("Content-Transfer-Encoding: \.* \n", self.email_brut)
+                header = re.split("Content-Transfer-Encoding: .* \n", email_brut)
 
             return header[0]
 
@@ -43,16 +44,17 @@ class Email:
         return :
             - return the body (str)
     '''
-    def extract_body(self):
+    def extract_body(self, txt):
+        email_brut = (txt + '.')[:-1]
         if self.type == 'html':
-            txt = re.split("</head>", self.email_brut)
+            txt = re.split("</head>", email_brut)
             return txt[1]
         else :
-            if re.search("Content-Transfer-Encoding:", self.email_brut) == None :
-                txt = re.split("Date : \.*\n", self.email_brut)
+            if re.search("Content-Transfer-Encoding:", email_brut) == None :
+                txt = re.split("Date:.*\n", email_brut)
             else :
-                txt = re.split("Content-Transfer-Encoding: \.* \n", self.email_brut)
-            return txt[1]
+                txt = re.split("Content-Transfer-Encoding: .* \n", email_brut)
+            return txt[2]
 
     '''
     extract_header()
@@ -60,31 +62,33 @@ class Email:
         return : 
             - return tab of all txt
     '''
-    def extract_txt(self):
+    def extract_txt(self, txt):
+        email_brut = (txt + '.')[:-1]
         if self.type == 'html':
-            txt = re.findall("<p class=\"MsoNormal\">(.+?)<o:p>", self.email_brut)
+            txt = re.findall("<p class=\"MsoNormal\">(.+?)<o:p>", email_brut)
             return txt
         else :
-            if re.search("Content-Transfer-Encoding:", self.email_brut) == None :
-                txt = re.split("Date : \.*\n", self.email_brut)
+            if re.search("Content-Transfer-Encoding:", email_brut) == None :
+                txt = re.split("Date:.*\n", email_brut)
             else :
-                txt = re.split("Content-Transfer-Encoding: \.* \n", self.email_brut)
-            return txt[1]
+                txt = re.split("Content-Transfer-Encoding: .* \n", email_brut)
+            return txt[2]
 
     # same as before but only return str
-    def extract_text_full(self):
+    def extract_text_full(self, txt):
+        email_brut = (txt + '.')[:-1]
         if self.type == 'html':
-            txt = re.findall("<p class=\"MsoNormal\">(.+?)<o:p>", self.email_brut)
+            txt = re.findall("<p class=\"MsoNormal\">(.+?)<o:p>", email_brut)
             tt = ''
             for i in txt:
                 tt += i
             return tt
         else :
-            if re.search("Content-Transfer-Encoding:", self.email_brut) == None:
-                txt = re.split("Date : \.*\n", self.email_brut)
+            if re.search("Content-Transfer-Encoding:", email_brut) == None:
+                txt = re.split("Date:.*\n", email_brut)
             else:
-                txt = re.split("Content-Transfer-Encoding: \.* \n", self.email_brut)
-            return txt[1]
+                txt = re.split("Content-Transfer-Encoding: .* \n", email_brut)
+            return txt[2]
 
     """
     find_language()
@@ -109,4 +113,20 @@ class Email:
 
         else :
             return None
+
+    # get header
+    def get_header(self):
+        return self.header
+
+    # get body
+    def get_body(self):
+        return self.body
+
+    # get text
+    def get_text(self):
+        return self.text
+
+    # get language
+    def get_language(self):
+        return self.lang
 
